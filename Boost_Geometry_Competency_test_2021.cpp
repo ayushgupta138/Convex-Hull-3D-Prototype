fe6e4b2336a0eea4dab4b82ae15b1ed2ac40e777
ayushgupta138 <ayush.gupta02071@gmail.com>
2021-03-28 00:59:15 +0530

    Add function for horizon edge calculation and add edge display functions for testing

--- Boost_Geometry_Competency_test_2021.cpp
+++ Boost_Geometry_Competency_test_2021.cpp
@@ -6,6 +6,8 @@
 #include<initializer_list>
 #include<random>
 #include<chrono> 
+#include<set>
+#include<map>
 
 #include<boost/geometry.hpp>
 
@@ -88,12 +90,25 @@ struct edge
 {
     facet<Point>* m_facet1, * m_facet2;
     vertex<Point>* m_v1, * m_v2;
+   
+    inline edge()
+    {}
+    
     inline edge(facet<Point>* f1, facet<Point>* f2, vertex<Point>* v1, vertex<Point>* v2)
     {
         m_facet1 = f1;
         m_facet2 = f2;
         m_v1 = v1;
         m_v2 = v2;
+    }
+    void print_edge()
+    {
+        std::cout << "Adjacent faces are :\n";
+        m_facet1->print_facet();
+        m_facet2->print_facet();
+        std::cout << "Adjacent vertices are:\n";
+        std::cout << wkt(m_v1->m_vertex) << "\n";
+        std::cout << wkt(m_v2->m_vertex) << "\n";
     }
 };
 
@@ -139,36 +154,29 @@ struct polyhedron
     inline facet<Point>* get_face()
     {
         BOOST_ASSERT((dim < m_face.size()));
-        auto it = boost::begin(m_face);
-        std::size_t count = dim;
-        while (count > 0)
-        {
-            count--;
-            it++;
-        }
-        facet<Point> f = *it;
-        return &f;
+        return &m_face[dim];
     }
 
     template<std::size_t dim>
     inline vertex<Point>* get_vertex()
     {
         BOOST_ASSERT((dim < m_vertex.size()));
-        auto it = boost::begin(m_vertex);
-        std::size_t count = dim;
-        while (count > 0)
-        {
-            count--;
-            it++;
-        }
-        vertex<Point> v = *it;
-        return &v;
+        return &m_vertex[dim];
     }
     inline void print_poly_facet() // function for testing purposes
     {
         for (auto it = boost::begin(m_face); it != boost::end(m_face); it++)
+        {
             it->print_facet();
+        }
    }
+    void print_edges()
+    {
+        for (auto it = boost::begin(m_edge); it != boost::end(m_edge); it++)
+        {
+            it->print_edge();
+        }
+    }
 };
 
 template
@@ -383,7 +391,7 @@ public:
         <
          typename Geometry
         >
-    inline void initialize_hull(Geometry const& geometry)
+    void initialize_hull(Geometry const& geometry)
     {
         BOOST_CONCEPT_ASSERT((concepts::MultiPoint<Geometry>));
         BOOST_ASSERT((geometry.size() > 3));
@@ -409,6 +417,7 @@ public:
         unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
         std::shuffle(boost::begin(m_unprocessed_points), boost::end(m_unprocessed_points), std::default_random_engine(seed));
         construct_initial_conflict_graph();
+        construct_hull();
     }
    template
         <
@@ -452,7 +461,7 @@ public:
         //m_polyhedron.print_poly_facet();
     }
     
-   inline void construct_initial_conflict_graph()
+   void construct_initial_conflict_graph()
    {
        initialize_conflict_graph();
        auto point_it = boost::begin(m_conflict_graph.m_point_list);
@@ -476,7 +485,7 @@ public:
    }
 
  
-   inline void initialize_conflict_graph()
+   void initialize_conflict_graph()
    {
        for (auto it = boost::begin(m_polyhedron.m_face); it != boost::end(m_polyhedron.m_face); it++)
        {
@@ -488,6 +497,64 @@ public:
            std::size_t index = boost::numeric_cast<std::size_t>(it - boost::begin(m_unprocessed_points));
            m_conflict_graph.m_point_list.push_back(std::make_pair(&m_unprocessed_points[index], std::vector<facet<Point>*>()));
        }
+   }
+
+   void construct_hull()
+   {
+       for (auto it = boost::rbegin(m_unprocessed_points); it != boost::rend(m_unprocessed_points); it++)
+       {
+           std::vector<edge<Point>*> edge_list;
+           create_horizon_edge_list(edge_list);
+           for (auto it : edge_list)
+               it->print_edge();
+       }
+   }
+
+   void create_horizon_edge_list(std::vector<edge<Point>*> & edge_list)
+   {
+       std::vector<facet<Point>*> visible_faces = m_conflict_graph.m_point_list.back().second;
+       std::set<facet<Point>*> face_set; // we can other use other efficient spatial index instead of set, but using set for simplicity 
+       for (auto it = boost::begin(visible_faces); it != boost::end(visible_faces); it++)
+       {
+           face_set.insert(*it);
+       }
+       for (auto it = boost::begin(m_polyhedron.m_edge); it != boost::end(m_polyhedron.m_edge); it++)
+       {
+           std::size_t count = 0;
+           if (face_set.find((*it).m_facet1) != face_set.end())
+           {
+               count++;
+           }
+           if (face_set.find((*it).m_facet2) != face_set.end())
+           {
+               count++;
+           }
+           std::size_t index = boost::numeric_cast<size_t>(it - boost::begin(m_polyhedron.m_edge));
+           if (count == 1)
+               edge_list.push_back(&m_polyhedron.m_edge[index]);
+       }
+       std::map<vertex<Point>*, std::size_t> vertex_map;
+       for (auto it = boost::begin(edge_list); it != boost::end(edge_list); it++)
+       {
+           std::size_t index = boost::numeric_cast<std::size_t>(it - boost::begin(edge_list));
+           vertex_map[(**it).m_v1] = index; // similarly we can use more efficient spatial index, but using map for simplicity
+       }
+       edge<Point> *edge1 = edge_list.front();
+       std::vector<edge<Point>*> edge_temp;
+       edge_temp.push_back(edge1);
+       edge<Point> *temp,*temp1;
+       do
+       {
+           temp = edge_temp.back();
+           temp1 = edge_list[vertex_map[(*temp).m_v2]];
+           edge_temp.push_back(temp1);
+       } while (temp1!=edge1);
+       edge_list.clear();
+       for (auto it = boost::begin(edge_temp); it != boost::end(edge_temp); it++)
+       {
+           edge_list.push_back(*it);
+       }
+       edge_list.push_back(edge1);
    }
 
    // should be declared as private, but public in this case for ease of testing
@@ -504,7 +571,7 @@ int main()
     typedef model::ring<point3d> rng;
     //std::cout << is_visible(point3d(0, 0, 1), point3d(1, 0, 0), point3d(0, 1, 0), point3d(0.33, 0.33, 0.34)) << "\n";
     mulpoly mul;
-    read_wkt("MULTIPOINT(0 0 0, 1 0 0 , 0 0 1 , 0 1 0 , -1 -1 0 ,-1 -1 -1)", mul);
+    read_wkt("MULTIPOINT(0 0 0, 1 0 0 , 0 0 1 , 0 1 0 , -1 -1 0 )", mul);
    
     convex_hull_3D<point3d> pt;
     pt.initialize_hull(mul);
