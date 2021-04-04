87cd6cb9260adc76ee6291ca651be524c0e12d58
ayushgupta138 <ayush.gupta02071@gmail.com>
2021-04-05 00:12:40 +0530

    Create header file

---
+++ convex_hull_3D.cpp
@@ -0,0 +1,30 @@
+#include "convex_hull_3D.hpp"
+#include<iostream>
+#include<chrono>
+#include<random>
+
+using namespace boost::geometry;
+using namespace std::chrono;
+
+int main()
+{
+    typedef model::d3::point_xyz<double> point3d;
+    typedef model::multi_point<point3d> mulpoly;
+    typedef model::ring<point3d> rng;
+    mulpoly mul;
+    int x, y, z;
+    for (int i = 0; i < 100; i++)
+    {
+        std::cin >> x >> y >> z;
+        append(mul, point3d(x, y, z));
+    }
+    std::cout << "\n\n";
+    //read_wkt("MULTIPOINT(0 0 0,1 1 1,1 2 3,0 0 2,0 2 0,0 2 2,1 1 4)", mul);
+    polyhedron<point3d> result;
+    auto start = high_resolution_clock::now();
+    result = convex_hull3D(mul);
+    auto stop = high_resolution_clock::now();
+    result.print_poly_facet();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    std::cout << "Time taken by 3D convex hull function : " << duration.count() << " ms\n";
+}
--- convex_hull_3D.hpp
+++ convex_hull_3D.hpp
@@ -1161,27 +1161,3 @@ polyhedron<model::d3::point_xyz<double>> convex_hull3D(Geometry1 const& input)
     hull.construct_hull();
     return hull.m_polyhedron;
 }
-
-int main()
-{
-    std::cout << "Hello World!\n";
-    typedef model::d3::point_xyz<double> point3d;
-    typedef model::multi_point<point3d> mulpoly;
-    typedef model::ring<point3d> rng;
-    mulpoly mul;
-    int x, y, z;
-    for (int i = 0; i < 100; i++)
-    {
-        std::cin >> x >> y >> z;
-        append(mul, point3d(x, y, z));
-    }
-    std::cout << "\n\n";
-    //read_wkt("MULTIPOINT(0 0 0,1 1 1,1 2 3,0 0 2,0 2 0,0 2 2,1 1 4)", mul);
-    polyhedron<point3d> result;
-    auto start = high_resolution_clock::now();
-    result = convex_hull3D(mul);
-    auto stop = high_resolution_clock::now();
-    result.print_poly_facet();
-    auto duration = duration_cast<milliseconds>(stop - start);
-    std::cout << "Time taken by 3D convex hull function : " << duration.count() << " ms\n";
-}
